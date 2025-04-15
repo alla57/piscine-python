0796d7b2fd636b60940b6b77439da738aeff9815
alla57 <a.boumediene01@gmail.com>
2025-04-15 19:27:15 +0200

    ex06 description added

--- day00/ex06/filterstring.py
+++ day00/ex06/filterstring.py
@@ -2,6 +2,14 @@ import sys
 from ft_filter import ft_filter
 
 def main():
+    """This program accepts two arguments: a string S and an integer N.
+    The program outputs a list of words from S that have a length greater than N
+    Words should separated from each other by space characters.
+    Strings do not contain any special characters. (Punctuation or invisible)
+    If the number of argument is different from 2, or if the type of any argument is wrong,
+    the program prints an AssertionError.
+    Not respecting these rules could lead to unexpected behavior.
+    """
     try:
         if len(sys.argv) != 3:
             raise AssertionError("the arguments are bad")
