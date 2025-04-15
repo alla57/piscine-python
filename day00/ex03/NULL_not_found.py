bb84e4a7abeaeace82dd2bc56306f9cca9a50d67
alla57 <a.boumediene01@gmail.com>
2025-04-15 10:06:21 +0200

    ex03 done

--- day00/ex03/NULL_not_found.py
+++ day00/ex03/NULL_not_found.py
@@ -1,2 +1,14 @@
 def NULL_not_found(object: any) -> int:
-    print(type(object))
+    if object == "Brian":
+        print("Type not Found")
+    elif object is None:
+        print("Nothing:", object, type(object))
+    elif isinstance(object, float):
+        print("Cheese:", object, type(object))
+    elif type(object) is int:
+        print("Zero:", object, type(object))
+    elif isinstance(object, str):
+        print("Empty:", type(object))
+    elif isinstance(object, bool):
+        print("Fake:", object, type(object))
+    return 1
