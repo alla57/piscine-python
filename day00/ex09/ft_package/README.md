97c5b065468f750234d15b7e3ee82049c2d84da8
alla57 <a.boumediene01@gmail.com>
2025-04-18 11:24:55 +0200

    ex09 done

---
+++ day00/ex09/ft_package/README.md
@@ -0,0 +1,8 @@
+# ft_package
+
+This is my first Python package!
+
+## Features
+
+- `greet(name)` - returns a greeting for the given name.
+- `add(a, b)` - returns the sum of two numbers.
---
+++ day00/ex09/ft_package/src/ft_package/core.py
@@ -0,0 +1,17 @@
+def count_in_list(list, item) -> int :
+    """Count the number of occurrences of an item in a list.
+
+    Parameters:
+        lst (list): The list in which to count occurrences.
+        item (any): The item to count.
+
+    Returns:
+        int: The number of times `item` appears in `lst`.
+
+    Example:
+        >>> count_in_list(["toto", "tata", "toto"], "toto")
+        2
+        >>> count_in_list(["toto", "tata", "toto"], "tutu")
+        0
+    """
+    return lst.count(item)
