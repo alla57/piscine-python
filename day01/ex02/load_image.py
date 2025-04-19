7c4a5a7f9c505c9fc98eadb5b13ae70f4f34708e
alla57 <a.boumediene01@gmail.com>
2025-04-19 11:48:38 +0200

    ex02 done

--- day01/ex02/load_image.py
+++ day01/ex02/load_image.py
@@ -14,4 +14,31 @@ def ft_load(path: str) -> np.ndarray | None:
             raise IsADirectoryError("Error the given path is not a file")
         if not os.access(path, os.R_OK):
             raise PermissionError(f"Error No permission for file: {path}")
-        
+
+        with Image.open(path) as img:
+            if not img.format or img.format != "JPEG":
+                raise AssertionError(f"Error: Invalid image format. "
+                                     f"Expected \"JPEG\", got {img.format}")
+            pixel_data = np.array(img)
+            print(f"The shape of image is: {pixel_data.shape}")
+            return pixel_data
+
+    except FileNotFoundError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+    except PermissionError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+    except IsADirectoryError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+    except TypeError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+    except AssertionError as e:
+        print(f"{type(e).__name__}: {e}", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {type(e).__name__}: {e}",
+              file=sys.stderr)
+        sys.exit(1)
