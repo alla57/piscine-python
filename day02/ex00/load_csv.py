bfec46f62ab06393c91d495a49d784feeccf7e78
alla57 <a.boumediene01@gmail.com>
2025-04-20 17:55:36 +0200

    ex00 wip

---
+++ day02/ex00/load_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame | None:
+    try:
+        
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {type(e).__name__}: {e}")
