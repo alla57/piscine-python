fb870da0b6ed8174d096ca5b1af01fa32425bd49
Alla eddine BOUMEDIENE <alboumed@f6r4s7.clusters.42paris.fr>
2025-05-14 17:00:18 +0200

    day 03 ex03 patched

--- day03/ex03/ft_calculator.py
+++ day03/ex03/ft_calculator.py
@@ -1,29 +1,30 @@
-import numpy as np
-
-
 class calculator:
-    def __init__(self, array):
-        self.array = np.array(array)
+    def __init__(self, lst):
+        self.lst = lst
 
     def __add__(self, object) -> None:
-        self.array += object
-        print(self.array.tolist())
+        for i in range(len(self.lst)):
+            self.lst[i] += object
+        print(self.lst)
         return
 
     def __mul__(self, object) -> None:
-        self.array *= object
-        print(self.array.tolist())
+        for i in range(len(self.lst)):
+            self.lst[i] *= object
+        print(self.lst)
         return
 
     def __sub__(self, object) -> None:
-        self.array -= object
-        print(self.array.tolist())
+        for i in range(len(self.lst)):
+            self.lst[i] -= object
+        print(self.lst)
         return
 
     def __truediv__(self, object) -> None:
         if object == 0:
             print("Error division by 0 impossible")
             return
-        self.array /= object
-        print(self.array.tolist())
+        for i in range(len(self.lst)):
+            self.lst[i] /= object
+        print(self.lst)
         return
