59e0bdb27f9819956c76b569337f2338e1c00298
Alla eddine BOUMEDIENE <alboumed@f6r2s2.clusters.42paris.fr>
2025-05-19 19:50:47 +0200

    day00 ex02 patched

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -1,16 +1,14 @@
 def all_thing_is_obj(object: any) -> int:
-    if not isinstance(object, int):
-        if isinstance(object, list):
-            print("List : ", end="")
-        elif isinstance(object, tuple):
-            print("Tuple : ", end="")
-        elif isinstance(object, set):
-            print("Set : ", end="")
-        elif isinstance(object, dict):
-            print("Dict : ", end="")
-        elif isinstance(object, str):
-            print(object, "is in the kitchen : ", end="")
-        print(type(object))
+    if isinstance(object, list):
+        print(f"List : {type(object)}")
+    elif isinstance(object, tuple):
+        print(f"Tuple : {type(object)}")
+    elif isinstance(object, set):
+        print(f"Set : {type(object)}")
+    elif isinstance(object, dict):
+        print(f"Dict : {type(object)}")
+    elif isinstance(object, str):
+        print(f"{object} is in the kitchen : {type(object)}")
     else:
         print("Type not found")
     return 42
